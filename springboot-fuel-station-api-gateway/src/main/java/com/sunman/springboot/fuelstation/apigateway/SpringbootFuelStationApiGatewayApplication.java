dba83b22db8a157a80f08cfb7ca3b9fd93553adc
bagwe <bagwe.manjiri25@gmail.com>
2023-05-04 22:07:28 -0400

    Adding Spring boot Gateway API for microservices

---
+++ springboot-fuel-station-api-gateway/src/main/java/com/sunman/springboot/fuelstation/apigateway/SpringbootFuelStationApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sunman.springboot.fuelstation.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFuelStationApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFuelStationApiGatewayApplication.class, args);
+	}
+
+}
---
+++ springboot-fuel-station-api-gateway/src/test/java/com/sunman/springboot/fuelstation/apigateway/SpringbootFuelStationApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunman.springboot.fuelstation.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFuelStationApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
